69f62ce95e30b09b1640bb62fd6e6440156a897b
ftatp <ftatp5901@gmail.com>
2017-11-22 17:19:33 +0900

    removed unnecessary stuffs

--- temporal-segment-networks/lib/dense_flow/include/easylogging++/samples/async/build.sh
+++
@@ -1,3 +0,0 @@
-compiler=icpc
-echo "$compiler prog.cpp -DELPP_EXPERIMENTAL_ASYNC -std=c++11 -lpthread -o prog && ./prog"
-$compiler prog.cpp -DELPP_EXPERIMENTAL_ASYNC -std=c++11 -lpthread -o prog && ./prog
