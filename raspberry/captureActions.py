592645bea24a27ea3c77a678774ad3836c3b2c90
damien911224 <damien911224@gmail.com>
2017-11-23 21:00:05 +0900

    complete motion detector

--- raspberry/captureActions.py
+++ raspberry/captureActions.py
@@ -6,7 +6,6 @@ import cv2
 import datetime
 import threading
 import time
-from numpy import linalg as LA
 
 
 
@@ -16,83 +15,69 @@ class Raspberry():
         self.server_ip_address = '127.0.0.1'
         self.server_port_number = 11224
 
-        self.frame_diff_threshold = 10
-        self.minimum_capture_count = 1000
-
-        self.web_camera_device_id = 0
-
         self.session_name = None
+
+        self.motion_detector = self.MotionDetector(self)
+        self.motion_detector_thread = threading.Thread(target=self.motion_detector.run, name='Motion Detector')
 
         self.raspberry_thread = threading.Thread(target=self.run, name='Raspberry')
         self.raspberry_thread.start()
 
-
     def run(self):
-        video_cap = cv2.VideoCapture(self.web_camera_device_id)
+        self.motion_detector_thread.start()
 
-        win_size = (64, 64)
-        block_size = (16, 16)
-        block_stride = (8, 8)
-        cell_size = (8, 8)
-        nbins = 9
-        deriv_aperture = 1
-        win_sigma = 4.0
-        histogram_norm_type = 0
-        L2_hys_threshold = 2.0000000000000001e-01
-        gamma_correction = 0
-        n_levels = 64
-
-        win_stride = (8, 8)
-        padding = (8, 8)
-        locations = ((10, 20),)
-
-        hog_descriptior = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size,
-                                            nbins, deriv_aperture, win_sigma, histogram_norm_type,
-                                            L2_hys_threshold, gamma_correction, n_levels)
+        while True:
+            time.sleep(0.3)
 
 
-        if video_cap.isOpened():
-            previous_frame = None
-            initialized = False
+    class MotionDetector():
 
-            while True:
-                ok, frame = video_cap.read()
-                if not ok:
-                    break
+        def __init__(self, raspberry):
+            self.raspberry = raspberry
 
-                if not initialized:
-                    frame = cv2.GaussianBlur(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), (21,21), 0)
-                    previous_hog_histograms = hog_descriptior.compute(frame, win_stride, padding, locations)
-                    normalization = LA.norm(previous_hog_histograms)
-                    previous_hog_histograms = np.divide(previous_hog_histograms, normalization)
-                    average_histogram = previous_hog_histograms
-                    initialized = True
-                    continue
+            self.frame_diff_threshold = 1.0
+            self.minimum_capture_count = 1000
+            self.web_cam_device_id = 0
 
-                frame = cv2.GaussianBlur(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), (21, 21), 0)
-
-                hog_histograms = hog_descriptior.compute(frame, win_stride, padding, locations)
-                normalization = LA.norm(hog_histograms)
-                hog_histograms = np.divide(hog_histograms, normalization)
-
-                frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-                hog_diff_abs_sum = np.sum(np.abs(hog_histograms - previous_hog_histograms))
-                cv2.accumulateWeighted(gray, avg, 0.5)
-
-                previous_hog_histograms = np.divide(previous_hog_histograms + hog_histograms, 2)
-
-                cv2.imshow('Hi', frame)
-                cv2.waitKey(1)
-                print hog_diff_abs_sum
+            self.countdown_to_stop = self.minimum_capture_count
+            self.no_moving = False
 
 
+        def run(self):
+            video_cap = cv2.VideoCapture(self.web_cam_device_id)
 
-                # print frame_diff_avg
-                # if frame_diff_avg >= self.frame_diff_threshold:
-                #     print 'Motion Detected!!'
-                #
-                # else:
-                #     print 'Silence --'
+            if video_cap.isOpened():
+                frame_avg = None
+                initialized = False
+
+                while True:
+                    ok, frame = video_cap.read()
+                    if not ok:
+                        break
+
+                    if not initialized:
+                        frame_gray = cv2.GaussianBlur(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), (21,21), 0)
+                        frame_avg = frame_gray
+                        initialized = True
+                        continue
+
+                    frame_gray = cv2.GaussianBlur(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), (21, 21), 0)
+                    frame_diff_avg = np.divide(np.sum(cv2.absdiff(frame_gray, cv2.convertScaleAbs(frame_avg))), len(frame.flatten()))
+
+                    frame_avg = np.divide(frame_avg + np.multiply(frame_gray, 0.5), 1.5)
+
+                    cv2.imshow('Hi', frame)
+                    cv2.waitKey(1)
+
+                    if frame_diff_avg >= self.frame_diff_threshold:
+                        self.countdown_to_stop = self.minimum_capture_count
+                    else:
+                        self.countdown_to_stop -= 1
+
+                    print self.countdown_to_stop
+
+                    if self.countdown_to_stop <= 0:
+                        self.no_moving = True
 
 
 
