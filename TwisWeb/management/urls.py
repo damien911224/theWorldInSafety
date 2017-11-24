c667071b1bdc67741b6d71f0a6b839f345ebdedd
ftatp <ftatp5901@gmail.com>
2017-11-24 13:12:56 +0900

    Modified for accessibity only for admin

--- TwisWeb/management/urls.py
+++ TwisWeb/management/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from management import views
+from management import views #, form
 
 urlpatterns = [
 	url(r'^$', views.overall.as_view(), name='overall'), #video list
 	url(r'^facility/$', views.FacilityLV.as_view(), name='Facility_list'),
 	url(r'^facility/(?P<pk>\d+)/$', views.FacilityDV.as_view(), name='Facility_detail'),
 	url(r'^video/(?P<pk>\d+)/$', views.VideoDV.as_view(), name='Video_detail'),
+	url(r'^receive/$', views.receive, name='receive'),
 ]
--- TwisWeb/management/views.py
+++ TwisWeb/management/views.py
@@ -3,10 +3,17 @@ from django.views.generic import ListView, DetailView
 
 from management.models import Video, Facility
 
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+#from .forms import UploadFileForm
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import user_passes_test
+from django.utils.decorators import method_decorator
+
+@method_decorator(user_passes_test(lambda u: u.is_superuser), name='dispatch')
 class overall(ListView):
 	template_name = 'overall.html'
 	model = Video
-
 	def get_queryset(self):
 		return Video.objects.all()[0:10]
 
@@ -21,3 +28,13 @@ class FacilityDV(DetailView):
 class VideoDV(DetailView):
 	template_name = 'video_detail.html'
 	model = Video
+
+@csrf_exempt
+def receive(request):
+	if request.method == 'POST':
+		print("Received file")
+		print("file:", request.FILES)
+		
+	#context = {}
+
+	return HttpResponse('Hello world\n')
