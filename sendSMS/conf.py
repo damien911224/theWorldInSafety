00ce1a5f5bb235d0056b11bdd7588bec37b64474
NaJaeMin7 <osial47@gmail.com>
2017-11-26 22:28:29 +0900

    sendSMS modified

--- sendSMS/conf.py
+++ sendSMS/conf.py
@@ -1,11 +1,11 @@
 import base64
 
 appid = 'twis'
-apikey = 'cb4eb398d21a11e794990cc47a1fcfae'
+apikey = 'api-key'
 address = 'api.bluehouselab.com'
 
-sender = '01091261777'
-receivers = ['01091261777', '01031345174', ]
+sender = 'phone-number'
+receivers = ['phone-number', ]
 content = u'Hi ru'  # url
 
 credential = "Basic "+base64.encodestring(appid+':'+apikey).strip()
