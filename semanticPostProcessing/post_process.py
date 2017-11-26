530acd3a7d6d38d6f4b84182d2ee9db8c6e0aa8e
NaJaeMin7 <osial47@gmail.com>
2017-11-27 00:11:35 +0900

    Modify confidence threshold in post_processor

--- semanticPostProcessing/post_process.py
+++ semanticPostProcessing/post_process.py
@@ -30,20 +30,20 @@ class SemanticPostProcessor:
                 continue
             result = self.tfnet.return_predict(img)
             for i in range(0, len(result), 1):
-                bounding_box = dict()
-                bounding_box['label'] = result[i]['label']
-                bounding_box['confidence'] = result[i]['confidence']
-                bounding_box['topleft_x'] = result[i]['topleft']['x']
-                bounding_box['topleft_y'] = result[i]['topleft']['y']
-                bounding_box['bottomright_x'] = result[i]['bottomright']['x']
-                bounding_box['bottomright_y'] = result[i]['bottomright']['y']
-                boxes.append(bounding_box)
-                if result[i]['label'] == 'Adult':
-                    adult = True
-                if result[i]['label'] == 'Child':
-                    child = True
-                if adult and child:
-                    flag = True
-
+                if result[i]['confidence'] > 0.15:
+                    bounding_box = dict()
+                    bounding_box['label'] = result[i]['label']
+                    bounding_box['confidence'] = result[i]['confidence']
+                    bounding_box['topleft_x'] = result[i]['topleft']['x']
+                    bounding_box['topleft_y'] = result[i]['topleft']['y']
+                    bounding_box['bottomright_x'] = result[i]['bottomright']['x']
+                    bounding_box['bottomright_y'] = result[i]['bottomright']['y']
+                    boxes.append(bounding_box)
+                    if result[i]['label'] == 'Adult':
+                        adult = True
+                    if result[i]['label'] == 'Child':
+                        child = True
+                    if adult and child:
+                        flag = True
             frame_semantics.append(boxes)
         return flag, frame_semantics
