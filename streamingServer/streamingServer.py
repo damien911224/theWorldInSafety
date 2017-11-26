9b14632d337b7ebf65b3caf254ab40ca92ea66d3
damien911224 <damien911224@gmail.com>
2017-11-26 19:26:53 +0900

    add testcode

--- streamingServer/streamingServer.py
+++ streamingServer/streamingServer.py
@@ -242,6 +242,7 @@ class StreamingServer():
                                 break
                             else:
                                 if len(frame_paths) >= 2:
+                                    print frame_paths
                                     frame_paths = frame_paths.sort()
                                 for frame_path in frame_paths:
                                     self.session_index = int(frame_path.split('_')[-1].split('.')[-2])
