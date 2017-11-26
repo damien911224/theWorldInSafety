eddb26edd7ce30f6269ef63d3238f83c145318fc
ftatp <ftatp5901@gmail.com>
2017-11-26 10:23:07 +0900

    Completed user's side

--- TwisWeb/usersite/views.py
+++ TwisWeb/usersite/views.py
@@ -7,12 +7,16 @@ from usersite.models import Uservideo
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
+#TODO: filter the 
+
+
 @method_decorator(login_required, name='dispatch')
 class FacilityLV(ListView):
 	template_name = 'facility_list.html'
 	model = Facility
-#	def get_queryset(self):
-#		return .objects.all()[0:10]
+
+	def get_queryset(self):
+		return Facility.objects.filter(Facility_name__in=self.request.user.groups.values_list('name', flat=True))
 
 
 @method_decorator(login_required, name='dispatch')
@@ -20,7 +24,7 @@ class FacilityDV(DetailView):
 	template_name = 'facility_detail.html'
 	model = Facility
 
-
+	
 @method_decorator(login_required, name='dispatch')
 class VideoDV(DetailView):
 	template_name = 'video_detail.html'
