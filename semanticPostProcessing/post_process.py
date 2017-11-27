61d759c1a6eb066bd1410d54b86fbf82b08f7781
NaJaeMin <osial47@gmail.com>
2017-11-27 22:24:39 +0900

    Modify drawing code

--- semanticPostProcessing/post_process.py
+++ semanticPostProcessing/post_process.py
@@ -30,7 +30,7 @@ class SemanticPostProcessor:
                 continue
             result = self.tfnet.return_predict(img)
             for i in range(0, len(result), 1):
-                if result[i]['confidence'] > 0.15:
+                if result[i]['confidence'] > 0.3:
                     bounding_box = dict()
                     bounding_box['label'] = result[i]['label']
                     bounding_box['confidence'] = result[i]['confidence']
