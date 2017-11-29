5d7358a3733c4a1a371cef2496d0e103745d3bd6
NaJaeMin <osial47@gmail.com>
2017-11-29 16:28:59 +0900

    remove confidence on bounding box

--- actionRecognition/twis/twisModelServer.py
+++ actionRecognition/twis/twisModelServer.py
@@ -1430,9 +1430,12 @@ class Secretary():
                                         cv2.rectangle(image, (semantic_topleft_x, semantic_topleft_y),
                                                       (semantic_bottomright_x, semantic_bottomright_y),
                                                       semantic_box_colors, semantic_thick)
-                                        cv2.putText(image, ("{0} {1:.2f}".format(semantic_label, semantic_confidence)),
+                                        cv2.putText(image, ("{0}".format(semantic_label)),
                                                     (semantic_topleft_x, semantic_topleft_y - 12), 2,
                                                     1.0, semantic_box_colors, 2)
+                                        # cv2.putText(image, ("{0} {1:.2f}".format(semantic_label, semantic_confidence)),
+                                        #             (semantic_topleft_x, semantic_topleft_y - 12), 2,
+                                        #             1.0, semantic_box_colors, 2)
 
                                 flow_x = cv2.resize(cv2.imread(view_frames[frame_index]['flows'][0],cv2.IMREAD_GRAYSCALE),
                                                     self.secretary.session.show_size, interpolation=cv2.INTER_AREA)
