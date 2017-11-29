e5dcb53e3295f12da0a896f3ad774f6dc70818b5
damien911224 <damien911224@gmail.com>
2017-11-29 16:23:25 +0900

    add reset

--- streamingServer/streamingServer.py
+++ streamingServer/streamingServer.py
@@ -417,9 +417,14 @@ class StreamingServer():
                                     print '{:10s}|{:15s}'.format('Controller', message_data.upper())
                                 break
                             elif message_data == 'reset':
-                                session_folders = glob.glob(os.path.join(self.streaming_server.save_folder), '*')
+                                session_folders = glob.glob(os.path.join(self.streaming_server.save_folder, '*'))
                                 for session_folder in session_folders:
                                     rmtree(session_folder, ignore_errors=True)
+
+
+                                with self.streaming_server.print_lock:
+                                    print '{:10s}|{:15s}'.format('Controller', message_data.upper())
+                                break
 
 
                 except socket.timeout:
