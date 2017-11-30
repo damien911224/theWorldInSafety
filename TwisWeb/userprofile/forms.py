215c8c72dfb8e37d5137bc4712ff1fd7770124c7
ftatp <ftatp5901@gmail.com>
2017-11-30 21:38:35 +0900

    Remodeled ajax file

--- TwisWeb/userprofile/forms.py
+++ TwisWeb/userprofile/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 
 from userprofile.models import UserProfile
+#from django.contrib.auth.models import User
+#
+#class UserForm(forms.ModelForm):
+#
+#	class Meta:
+#		model = User
+#		fields = ()
 
 class UserProfileForm(forms.ModelForm):
 
