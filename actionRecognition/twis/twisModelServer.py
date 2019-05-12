ec9193f06a7999a63c2b9791bbe385c5a4faad3e
Ji.hwan Kim <damien911224@gmail.com>
2019-05-13 03:00:08 +0900

    Update twisModelServer.py
    modify model server

--- actionRecognition/twis/twisModelServer.py
+++ actionRecognition/twis/twisModelServer.py
@@ -181,10 +181,10 @@ class Session():
         self.print_lock = Lock()
         self.average_delay = 0.0
 
-        self.server_ip_address = '13.124.183.55'
+        self.server_ip_address = ''
         self.server_port_number = 8888
 
-        self.client_host_name = '192.168.1.101'
+        self.client_host_name = ''
         self.client_port_number = random.sample(range(10000, 20000, 1), 1)[0]
 
         self.extractor = Extractor(self)
